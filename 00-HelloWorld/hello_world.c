921bfee871f057df3f3052d4ec6e327c7a05e941
acoaquiraticona <acoaquiraticona@frba.utn.edu.ar>
2026-04-26 16:26:03 -0300

    Resolucion final TPO

---
+++ 00-HelloWorld/hello_world.c
@@ -0,0 +1,10 @@
+/* Qué: Programa que imprime "Hello, World!"
+ * Quiénes: Coaquira Ticona Alejandro
+ * Cuándo: 2026-04-26
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
